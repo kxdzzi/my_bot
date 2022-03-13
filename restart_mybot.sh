bb88f58fb951d865ac736ff2738a5780417b8f05
ermaozi <admin@ermao.net>
2022-03-13 20:53:41 +0800

    修改重新启动脚本

--- restart_mybot.sh
+++ restart_mybot.sh
@@ -1,7 +1,7 @@
-screen -S mybot -X quit;screen -dmS mybot bash -c 'cd ~/mini_jx3_bot;git pull;python3.9 bot.py'
+screen -S mybot -X quit;screen -dmS my_bot bash -c 'cd ~/my_bot;git pull;python3.9 bot.py'
 
 for i in $(ls ~/go-cqhttp/);do
     screen -S $i -X quit;screen -dmS $i bash -c "cd ~/go-cqhttp/$i;./go-cqhttp faststart"
 done
 
-screen -dmS update bash -c 'cd ~/mini_jx3_bot;python3.9 server_update.py'
+screen -dmS update bash -c 'cd ~/my_bot;python3.9 server_update.py'
