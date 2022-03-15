b2a8adb463726008ba6172cd661455459a713259
ermaozi <admin@ermao.net>
2022-03-15 08:45:09 +0800

    重启脚本增加重启数据库

--- restart_mybot.sh
+++ restart_mybot.sh
@@ -1,7 +1,12 @@
+sudo docker restart mongo
+echo 数据库重启成功
 screen -S my_bot -X quit;screen -dmS my_bot bash -c 'cd ~/my_bot;git pull;python3.9 bot.py'
+echo 框架重启成功
 
 for i in $(ls ~/go-cqhttp/);do
     screen -S $i -X quit;screen -dmS $i bash -c "cd ~/go-cqhttp/$i;./go-cqhttp faststart"
 done
+echo 机器人重启成功
 
 screen -dmS update bash -c 'cd ~/my_bot;python3.9 server_update.py'
+echo 重启成功!
